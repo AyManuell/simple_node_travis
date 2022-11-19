440262661d9dc9e2369f2fe5c17190100b3cb7aa
AyManuell <nuelayo97@gmail.com>
2022-11-19 13:00:00 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const FAVORITE_INSTRUCTOR = process.env.NAME 
+
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function main() {
+  while(true) {
+    console.log(`${FAVORITE_INSTRUCTOR} rock!`);
+    //console.log("Microservices rock!");
+    await sleep(5000);
+  }
+}
+
+main();
